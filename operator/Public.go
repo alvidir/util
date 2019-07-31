9a65143d8e5598ce799bdce87c15135f3f19c6d3
HectorMRC <thehector1593@gmail.com>
2019-07-31 22:12:14 +0200

    updates

--- operator/Public.go
+++ operator/Public.go
@@ -2,11 +2,22 @@ package operator
 
 type procedure func(...interface{}) error
 
-// Ternary operates as an ternary conditional operator for
+// Ternary operates as an ternary conditional operator between
+// two actions.
 func Ternary(cond bool, doif procedure, ifnot procedure) error {
 	if cond {
 		return doif()
 	}
 
 	return ifnot()
+}
+
+// Switch switches action to gorutine if cond is true; otherwise
+// keeps in the same fiber
+func Switch(cond bool, action procedure) {
+	if cond {
+		go action()
+	} else {
+		action()
+	}
 }
