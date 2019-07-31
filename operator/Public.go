f9c1b04dcf7198f6291455a4f74b12ef99152ffd
HectorMRC <thehector1593@gmail.com>
2019-08-01 01:00:41 +0200

    updates

--- operator/Public.go
+++ operator/Public.go
@@ -1,15 +1,5 @@
 package operator
 
-// Ternary operates as an ternary conditional operator between
-// two actions.
-func Ternary(cond bool, runmeiftrue func(), runmeifalse func()) {
-	if cond {
-		runmeiftrue()
-	} else {
-		runmeifalse()
-	}
-}
-
 // Switch switches an action to gorutine if cond is true; otherwise
 // keeps in the same fiber
 func Switch(cond bool, action func()) {
