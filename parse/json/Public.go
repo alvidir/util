37c961c88b9b958a8727c2fc836070217b72657e
Hèctor Morales Carnicé <hector.morales.carnice@est.fib.upc.edu>
2019-08-01 11:44:23 +0200

    Update Public.go

--- parse/json/Public.go
+++ parse/json/Public.go
@@ -3,25 +3,15 @@ package json
 import (
 	"encoding/json"
 
-	reader "github.com/alvidir/util/stream/reader"
-	writer "github.com/alvidir/util/stream/writer"
+	parse "github.com/alvidir/util/parse"
 )
 
 // Unmarshal decodes an json definition to interface object
 func Unmarshal(filepath string, stream interface{}) (err error) {
-	var content []byte
-	if content, err = reader.Read(filepath); err == nil {
-		err = json.Unmarshal(content, stream)
-	}
-	return
+	return parse.Unmarshal(filepath, stream, json.Unmarshal)
 }
 
 // Marshal encode an interface object to corresponding json definition
 func Marshal(filepath string, content interface{}) (err error) {
-	var data []byte
-	if data, err = json.Marshal(content); err == nil {
-		err = writer.Write(filepath, data, false, true)
-	}
-
-	return
+	return parse.Marshal(filepath, content, json.Marshal)
 }
