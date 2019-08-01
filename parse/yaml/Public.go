81fce1e64dd4206e3ec19dbb5b0bd25b657214f1
Hèctor Morales Carnicé <hector.morales.carnice@est.fib.upc.edu>
2019-08-01 11:40:00 +0200

    Update Public.go

--- parse/yaml/Public.go
+++ parse/yaml/Public.go
@@ -1,29 +1,16 @@
 package yaml
 
 import (
-	reader "github.com/alvidir/util/stream/reader"
-	writer "github.com/alvidir/util/stream/writer"
+	parse "github.com/alvidir/util/parse"
 	"gopkg.in/yaml.v2"
 )
 
 // Unmarshal decodes an yaml definition to interface object
 func Unmarshal(filepath string, manifest interface{}) (err error) {
-	var content []byte
-	if content, err = reader.Read(filepath); err == nil {
-		//Si el fitxer s'ha pogut obrir satisfactoriament
-		err = yaml.Unmarshal(content, manifest)
-	}
-
-	return
+	return parse.UnmarshalStrategy(filepath, manifest, yaml.Unmarshal)
 }
 
 // Marshal encode an interface object to corresponding yaml definition
 func Marshal(filepath string, content interface{}) (err error) {
-	var data []byte
-	if data, err = yaml.Marshal(content); err == nil {
-		//Si la conversió ha sigut satisfactoria
-		err = writer.Write(filepath, data, false, true)
-	}
-
-	return
+	return parse.Marshal(filepath, content, yaml.Marshal)
 }
