e887da4f958e3a1b6634d7a1f5f4a567d3388b85
HectorMRC <thehector1593@gmail.com>
2019-08-01 21:57:29 +0200

    updates

--- parse/json/Public.go
+++ parse/json/Public.go
@@ -8,10 +8,10 @@ import (
 
 // Unmarshal decodes an json definition to interface object
 func Unmarshal(filepath string, stream interface{}) (err error) {
-	return parse.Unmarshal(filepath, stream, json.Unmarshal)
+	return parse.UnmarshalStrategy(filepath, stream, json.Unmarshal)
 }
 
 // Marshal encode an interface object to corresponding json definition
 func Marshal(filepath string, content interface{}) (err error) {
-	return parse.Marshal(filepath, content, json.Marshal)
+	return parse.MarshalStrategy(filepath, content, json.Marshal)
 }
