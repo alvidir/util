21cfc943c9fc11076ff73148a875001eb51c8f26
Hèctor Morales Carnicé <hector.morales.carnice@est.fib.upc.edu>
2019-08-01 11:35:12 +0200

    Update strategy.go

--- parse/strategy.go
+++ parse/strategy.go
@@ -5,6 +5,9 @@ import (
 	"github.com/alvidir/util/stream/writer"
 )
 
+type unmarshal func([]byte, interface{}) error
+type marshal func(interface{}) ([]byte, error)
+
 // An Encoder represents an object able to be parsed
 type Encoder interface {
 	Unmarshal([]byte, interface{}) error
@@ -12,20 +15,20 @@ type Encoder interface {
 }
 
 // UnmarshalStrategy decodes an encoded definition to interface object
-func UnmarshalStrategy(filepath string, stream interface{}, code Encoder) (err error) {
+func UnmarshalStrategy(path string, stream interface{}, parser unmarshal) (err error) {
 	var content []byte
-	if content, err = reader.Read(filepath); err == nil {
-		err = code.Unmarshal(content, stream)
+	if content, err = reader.Read(path); err == nil {
+		err = parser(content, stream)
 	}
 
 	return
 }
 
 // MarshalStrategy encode an interface object to corresponding coding definition
-func MarshalStrategy(filepath string, content interface{}, code Encoder) (err error) {
+func MarshalStrategy(path string, content interface{}, parser marshal) (err error) {
 	var data []byte
-	if data, err = code.Marshal(content); err == nil {
-		err = writer.Write(filepath, data, false, true)
+	if data, err = parser(content); err == nil {
+		err = writer.Write(path, data, false, true)
 	}
 
 	return
