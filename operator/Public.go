92730880cc833de1ba90c018a42eb91ff90d90e4
HectorMRC <thehector1593@gmail.com>
2019-08-03 00:55:18 +0200

    updates

--- operator/Public.go
+++ operator/Public.go
@@ -1,5 +1,9 @@
 package operator
 
+import (
+	"math"
+)
+
 // Switch switches an action to gorutine if cond is true; otherwise
 // keeps in the same fiber
 func Switch(cond bool, action func()) {
@@ -8,4 +12,15 @@ func Switch(cond bool, action func()) {
 	} else {
 		action()
 	}
+}
+
+// Normalize change the state of a float to range [0.0, 1.0)
+func Normalize(in float64) float64 {
+	if abs := math.Abs(in); abs > 1 {
+		i64 := uint64(abs)
+		f64 := float64(i64)
+		return abs - f64
+	}
+
+	return in
 }
