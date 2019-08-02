b9d18ebd520dc3baa5940e1999a51943e343f010
HectorMRC <thehector1593@gmail.com>
2019-08-03 00:55:35 +0200

    updates

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/alvidir/util
 
 go 1.12
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	golang.org/x/tools v0.0.0-20190802220118-1d1727260058 // indirect
+	gopkg.in/yaml.v2 v2.2.2
+)
--- rand/Random.go
+++ rand/Random.go
@@ -2,11 +2,26 @@ package rand
 
 import (
 	"math/rand"
+
+	operator "github.com/alvidir/util/operator"
 	time "github.com/alvidir/util/time"
 )
 
-// Randomize genera un valor aleatori sense signe de 64 bits
+// Random builds a brand new randomizer
+func Random() *rand.Rand {
+	seed := time.Unix()
+	source := rand.NewSource(seed)
+	return rand.New(source)
+}
+
+// Randomize gives a pseudo-ramdom unsigned value 64 bits lenght
 func Randomize() uint64 {
-	rand.Seed(time.Unix())
-	return rand.Uint64()
+	return Random().Uint64()
+}
+
+// Probability returns true or false pseudo-ramdonly under the
+// given probability
+func Probability(prob float64) bool {
+	norma := operator.Normalize(prob)
+	return Random().Float64() <= norma
 }
