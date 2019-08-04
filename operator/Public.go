969de92335971fa1eff00e426da8ca276d15373e
HectorMRC <thehector1593@gmail.com>
2019-08-04 20:49:36 +0200

    updates

--- operator/Public.go
+++ operator/Public.go
@@ -4,6 +4,15 @@ import (
 	"math"
 )
 
+// Abs returns the absolut value for a given int
+func Abs(n int) int {
+	if n < 0 {
+		return -n
+	} else {
+		return n
+	}
+}
+
 // Switch switches an action to gorutine if cond is true; otherwise
 // keeps in the same fiber
 func Switch(cond bool, action func()) {
