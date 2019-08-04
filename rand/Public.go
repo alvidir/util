c2bf5c7558bf1c8992543fa952d26f0912cdf7c5
HectorMRC <thehector1593@gmail.com>
2019-08-04 18:37:46 +0200

    updates

--- rand/Public.go
+++ rand/Public.go
@@ -12,16 +12,21 @@ func random() *rand.Rand {
 	return New(seed)
 }
 
-// Randomize gives a pseudo-random unsigned value 64 bits lenght
-func Randomize() uint64 {
+// Uint gives a pseudo-random unsigned value 64 bits lenght
+func Uint() uint {
+	return uint(random().Int())
+}
+
+// Uint64 gives a pseudo-random unsigned value 64 bits lenght
+func Uint64() uint64 {
 	return random().Uint64()
 }
 
 // Entropy returns true or false pseudo-randomly under the
 // given float as probability of true
 func Entropy(frac float64) bool {
-	limit := operator.Normalize(frac)
+	bounds := operator.Normalize(frac)
 	cursor := random().Float64()
 
-	return cursor <= limit
+	return cursor <= bounds
 }
