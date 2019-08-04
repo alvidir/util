d2090cd931dc639ae71b20359a7165346b4f43e2
HectorMRC <thehector1593@gmail.com>
2019-08-04 19:46:41 +0200

    updates

--- operator/Public.go
+++ operator/Public.go
@@ -14,9 +14,9 @@ func Switch(cond bool, action func()) {
 	}
 }
 
-// Normalize change the state of a float to range [0.0, 1.0)
+// Normalize change the state of a float to range [0.0, 1.0]
 func Normalize(f64 float64) float64 {
-	if f64 = math.Abs(f64); f64 > 1 {
+	if f64 = math.Abs(f64); f64 > 1. {
 		_, f64 = math.Modf(f64)
 	}
 
