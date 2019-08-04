c2726941d20c56e5553e662fb2d4e14be20f3414
HectorMRC <thehector1593@gmail.com>
2019-08-04 18:48:04 +0200

    updates

--- rand/Public.go
+++ rand/Public.go
@@ -22,6 +22,16 @@ func Uint() uint {
 	return uint(random().Int())
 }
 
+// Int64 gives a pseudo-random value 64 bits lenght
+func Int64() int64 {
+	unsig := random().Int63()
+	if Entropy(0.5) { // fifty-fifty to become negative
+		unsig *= -1
+	}
+
+	return unsig
+}
+
 // Uint64 gives a pseudo-random unsigned value 64 bits lenght
 func Uint64() uint64 {
 	return random().Uint64()
