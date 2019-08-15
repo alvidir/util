21dcaab74ae0b316040714193bc395decb92f2c6
HectorMRC <thehector1593@gmail.com>
2019-08-15 19:19:01 +0200

    updates

--- algorithm/Public.go
+++ algorithm/Public.go
@@ -1,1 +1,31 @@
 package algorithm
+
+import "fmt"
+
+// Nav gets recursively a deep value inside a map, considering the value for each key in args is already
+// an other map of interfaces, navigable as well
+func Nav(content map[interface{}]interface{}, args ...interface{}) (value interface{}, err error) {
+	if len(args) == 0 {
+		value = content
+		return
+	}
+
+	if content == nil {
+		err = fmt.Errorf("Cannot navigate the current content map %v", content)
+		return
+	}
+
+	key, exists := args[0], false
+	if value, exists = content[key]; !exists {
+		err = fmt.Errorf("Cannot get resource for provided key %v", key)
+		return
+	}
+
+	if parse, ok := value.(map[interface{}]interface{}); ok {
+		value, err = Nav(parse, args[1:])
+	} else if len(args) > 1 {
+		err = fmt.Errorf("Cannot complete navigability, %v is not a map", value)
+	}
+
+	return
+}
