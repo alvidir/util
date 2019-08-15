b522debbac0f73c36fd3efec165fc2311407367f
HectorMRC <thehector1593@gmail.com>
2019-08-15 18:44:57 +0200

    updates

---
+++ hash/Public.go
@@ -0,0 +1,16 @@
+package hash
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"strings"
+)
+
+// FormatStr256 gives an string that represents the sum256 of all arguments in base 16
+func FormatStr256(args ...string) (hash string) {
+	hash = strings.Join(args, ".")
+	argot := []byte(hash)
+	forma := sha256.Sum256(argot)
+
+	return fmt.Sprintf("%x", forma)
+}
