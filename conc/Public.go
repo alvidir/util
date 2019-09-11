3f01a664681536fcaad6dc0014f07234cef4dc85
HectorMRC <thehector1593@gmail.com>
2019-09-11 02:01:56 +0200

    updates

---
+++ conc/Public.go
@@ -0,0 +1,11 @@
+package conc
+
+// Switch switches an action to gorutine if cond is true; otherwise
+// keeps in the same fiber
+func Switch(cond bool, action func()) {
+	if cond {
+		go action()
+	} else {
+		action()
+	}
+}
--- operator/Public.go
+++ operator/Public.go
@@ -21,16 +21,6 @@ func Clone(original interface{}) interface{} {
 	return clone.Interface()
 }
 
-// Switch switches an action to gorutine if cond is true; otherwise
-// keeps in the same fiber
-func Switch(cond bool, action func()) {
-	if cond {
-		go action()
-	} else {
-		action()
-	}
-}
-
 // Normalize change the state of a float to range [0.0, 1.0]
 func Normalize(f64 float64) float64 {
 	if f64 = math.Abs(f64); f64 > 1. {
