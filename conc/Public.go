a0ac794f14ecba01e99970ff6534a0969a34eaf2
HectorMRC <thehector1593@gmail.com>
2019-09-17 21:32:20 +0200

    updates

--- conc/Public.go
+++ conc/Public.go
@@ -1,5 +1,7 @@
 package conc
 
+import "sync"
+
 // Switch switches an action to gorutine if cond is true; otherwise
 // keeps in the same fiber
 func Switch(cond bool, action func()) {
@@ -8,4 +10,12 @@ func Switch(cond bool, action func()) {
 	} else {
 		action()
 	}
+}
+
+// ClearMap erase all content in map m
+func ClearMap(m *sync.Map) {
+	m.Range(func(key interface{}, value interface{}) bool {
+		m.Delete(key)
+		return true
+	})
 }
