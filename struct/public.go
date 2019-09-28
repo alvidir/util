7eb86aa6fb1e2526546985a9f2806b3a6cb1f997
HectorMRC <thehector1593@gmail.com>
2019-09-29 00:53:36 +0200

    updates

--- struct/public.go
+++ struct/public.go
@@ -4,26 +4,29 @@ import (
 	"fmt"
 )
 
+// An InterMap represents a bilateral map of unknown types
+type InterMap map[interface{}]interface{}
+
 // Nav gets recursively a deep value inside a map, considering the values for each key in args are already
 // other maps of interfaces, navigables as well
-func Nav(content map[interface{}]interface{}, args ...interface{}) (value interface{}, err error) {
-	if len(args) == 0 {
-		return content, nil
-	}
-
-	if content == nil {
-		err = fmt.Errorf("Cannot navigate the current content map %v", content)
+func (imap InterMap) Nav(args ...interface{}) (value interface{}, err error) {
+	if imap == nil {
+		err = fmt.Errorf("Cannot navigate the current content map %v", imap)
 		return
 	}
 
+	if len(args) == 0 {
+		return imap, nil
+	}
+
 	key, exists := args[0], false
-	if value, exists = content[key]; !exists {
+	if value, exists = imap[key]; !exists {
 		err = fmt.Errorf("Cannot get resource for provided key %v", key)
-		return content, err
+		return imap, err
 	}
 
 	if parse, ok := value.(map[interface{}]interface{}); ok {
-		value, err = Nav(parse, args[1:])
+		value, err = imap.Nav(parse, args[1:])
 	} else if len(args) > 1 {
 		err = fmt.Errorf("Cannot complete navigability, %v is not a map", value)
 	}
