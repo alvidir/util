958f7fab3eab6231291db9b87ec1e8cd19bcc0a9
HectorMRC <thehector1593@gmail.com>
2019-10-05 22:46:01 +0200

    updates

--- parse/json/public.go
+++ parse/json/public.go
@@ -3,17 +3,17 @@ package json
 import (
 	"encoding/json"
 
-	coder "github.com/alvidir/util/parse/encoder"
+	parse "github.com/alvidir/util/parse"
 )
 
 // Unmarshal decodes an json definition to interface object
 func Unmarshal(filepath string, manifest interface{}) (err error) {
-	var marshal coder.Unmarshal = json.Unmarshal
+	var marshal parse.Unmarshal = json.Unmarshal
 	return marshal.Path(filepath, manifest)
 }
 
 // Marshal encode an interface object to corresponding json definition
 func Marshal(filepath string, content interface{}) (err error) {
-	var marshal coder.Marshal = json.Marshal
+	var marshal parse.Marshal = json.Marshal
 	return marshal.Path(filepath, content)
 }
