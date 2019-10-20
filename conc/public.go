6c3a8e66e373cedfe8d561d3efffac63f3cec681
HectorMRC <thehector1593@gmail.com>
2019-10-20 19:32:56 +0200

    updates

--- conc/public.go
+++ conc/public.go
@@ -1,17 +1,25 @@
 package conc
 
 import (
+	"os"
 	"sync"
 )
 
-// Switch switches an action to gorutine if cond is true; otherwise
-// keeps in the same fiber
-func Switch(cond bool, action func()) {
-	if cond {
-		go action()
-	} else {
+// Switch switches an action to gorutine and returns the new process pid if cond is true;
+// otherwise keeps in the same fiber
+func Switch(cond bool, action func()) int {
+	if !cond {
 		action()
+		return 0
 	}
+
+	pid := make(chan int)
+	go func(chann chan<- int) {
+		pid <- os.Getpid()
+		action()
+	}(pid)
+
+	return <-pid
 }
 
 // ClearMap erase all content in the sync.Map m
