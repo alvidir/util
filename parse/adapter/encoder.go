50db725ba86b3bf872d02d213a2baaf391c9566d
Hèctor <thehector1593@gmail.com>
2020-01-25 23:11:14 +0100

    updates

--- parse/adapter/encoder.go
+++ parse/adapter/encoder.go
@@ -1,19 +1,19 @@
 package adapter
 
-type adapter struct {
+type Adaptee struct {
 	X_Marshal   Marshal
 	X_Unmarshal Unmarshal
 }
 
-func (parse *adapter) Marshal() Marshal {
+func (parse *Adaptee) Marshal() Marshal {
 	return parse.X_Marshal
 }
 
-func (parse *adapter) Unmarshal() Unmarshal {
+func (parse *Adaptee) Unmarshal() Unmarshal {
 	return parse.X_Unmarshal
 }
 
 // New builds a new Encoder
 func New(marshal Marshal, unmarshal Unmarshal) Adapter {
-	return &adapter{X_Marshal: marshal, X_Unmarshal: unmarshal}
+	return &Adaptee{X_Marshal: marshal, X_Unmarshal: unmarshal}
 }
