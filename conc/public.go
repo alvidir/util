61eeb2a31875587b7eaf0bf48da0f47e15859d63
Hèctor <thehector1593@gmail.com>
2020-01-25 21:05:47 +0100

    updates

--- conc/public.go
+++ conc/public.go
@@ -27,3 +27,35 @@ func Switch(cond bool, action func()) int {
 func Coherence(lock bool, lockers ...sync.Locker) {
 
 }
+
+// Merge converts a list of channels to a single channel
+// For more information about Merge function go to https://blog.golang.org/pipelines
+func Merge(done <-chan struct{}, cs ...<-chan interface{}) <-chan interface{} {
+	var wg sync.WaitGroup
+	output := make(chan interface{})
+
+	wg.Add(len(cs))
+	for _, c := range cs {
+		// Start an output goroutine for each input channel in cs.  output
+		// copies values from c to out until c or done is closed, then calls
+		// wg.Done.
+		go func(in <-chan interface{}, out chan<- interface{}, done <-chan struct{}, wg *sync.WaitGroup) {
+			defer wg.Done()
+			for got := range in {
+				select {
+				case out <- got:
+				case <-done:
+					return
+				}
+			}
+		}(c, output, done, &wg)
+	}
+
+	// Start a goroutine to close out once all the output goroutines are
+	// done.  This must start after the wg.Add call.
+	go func() {
+		wg.Wait()
+		close(output)
+	}()
+	return output
+}
