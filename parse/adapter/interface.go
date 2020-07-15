24d315fb1c13119663060266fad2a843d7f7c13e
Hèctor <thehector1593@gmail.com>
2020-07-15 23:27:53 +0200

    updates

--- parse/adapter/interface.go
+++ parse/adapter/interface.go
@@ -2,6 +2,6 @@ package adapter
 
 // An Adapter represents a set of de/encoder for an specific format
 type Adapter interface {
-	Encoder() Marshaler
-	Decoder() Unmarshaler
+	Encoder() *Marshaler
+	Decoder() *Unmarshaler
 }
