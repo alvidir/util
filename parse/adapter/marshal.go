ec28824b3cc0cc384f3f31267c246bb49244466e
Hèctor <thehector1593@gmail.com>
2020-07-15 23:29:38 +0200

    updates

--- parse/adapter/marshal.go
+++ parse/adapter/marshal.go
@@ -4,18 +4,18 @@ import "github.com/alvidir/util/stream/writer"
 
 type Marshal func(interface{}) ([]byte, error)
 
-// MarshalAdapter represents a encoder
-type MarshalAdapter struct {
+// Marshaler represents a encoder
+type Marshaler struct {
 	Fx Marshal
 }
 
 // Marshal encode an interface object into an array of bytes
-func (adapter *MarshalAdapter) Marshal(i interface{}) ([]byte, error) {
+func (adapter *Marshaler) Marshal(i interface{}) ([]byte, error) {
 	return adapter.Fx(i)
 }
 
 // Path encode an interface object to corresponding path
-func (adapter *MarshalAdapter) Path(path string, content interface{}) (err error) {
+func (adapter *Marshaler) Path(path string, content interface{}) (err error) {
 	var data []byte
 	if data, err = adapter.Fx(content); err == nil {
 		err = writer.Write(path, data, false, true)
