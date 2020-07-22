a0b2a9dabae5f08a070335470b42aa6f5d9cdefd
Hèctor <thehector1593@gmail.com>
2020-07-22 21:09:16 +0200

    Slice methods added

--- method/public.go
+++ method/public.go
@@ -31,3 +31,33 @@ func ToUintptr(v interface{}) (ptr uintptr, err error) {
 	ptr = uintptr(pseudo)
 	return
 }
+
+// Find takes an slice of any type and finds the position of a provided value
+func Find(slice []interface{}, val interface{}) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+// FindInt takes an slice of ints and finds the position of a provided value
+func FindInt(slice []int, val int) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
+
+// FindString takes an slice of strings and finds the position of a provided value
+func FindString(slice []string, val string) (int, bool) {
+	for i, item := range slice {
+		if item == val {
+			return i, true
+		}
+	}
+	return -1, false
+}
