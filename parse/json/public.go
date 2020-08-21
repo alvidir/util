b646db97cf3d08231dd6b0e2e15b80de224cf9c0
Hector <hector.morales.carnice@gmail.com>
2020-08-21 17:14:33 +0200

    updates

--- parse/json/public.go
+++ parse/json/public.go
@@ -9,23 +9,6 @@ import (
 	adapt "github.com/alvidir/util/parse/adapter"
 )
 
-// UnmarshalResponse decodes a json response from a source into target interface
-func UnmarshalResponse(ctx context.Context, addr string, target interface{}) (err error) {
-	client := &http.Client{}
-	if deadline, ok := ctx.Deadline(); ok {
-		timeout := time.Until(deadline)
-		client.Timeout = timeout
-	}
-
-	var res *http.Response
-	if res, err = client.Get(addr); err != nil {
-		return
-	}
-
-	defer res.Body.Close()
-	return json.NewDecoder(res.Body).Decode(target)
-}
-
 // Unmarshal decodes an json definition to interface object
 func Unmarshal(filepath string, manifest interface{}) (err error) {
 	unmarshal := adapt.NewUnmarshaler(json.Unmarshal)
@@ -43,4 +26,21 @@ func Adapter() adapt.Adapter {
 	marshal := *adapt.NewMarshaler(json.Marshal)
 	unmarshal := *adapt.NewUnmarshaler(json.Unmarshal)
 	return adapt.NewEncoder(marshal, unmarshal)
+}
+
+// UnmarshalResponse decodes a json response from a source into target interface
+func UnmarshalResponse(ctx context.Context, addr string, target interface{}) (err error) {
+	client := &http.Client{}
+	if deadline, ok := ctx.Deadline(); ok {
+		timeout := time.Until(deadline)
+		client.Timeout = timeout
+	}
+
+	var res *http.Response
+	if res, err = client.Get(addr); err != nil {
+		return
+	}
+
+	defer res.Body.Close()
+	return json.NewDecoder(res.Body).Decode(target)
 }
