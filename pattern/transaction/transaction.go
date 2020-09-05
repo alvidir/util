4744bb66cc543bc62aebc0b11538e2a0493aa0f3
Hèctor Morales <thehector1593@gmail.com>
2020-09-05 18:28:06 +0200

    transaction added & solved bug

--- pattern/transaction/transaction.go
+++ pattern/transaction/transaction.go
@@ -7,30 +7,41 @@ import (
 
 // transaction adapts a body into an executable transaction
 type transaction struct {
-	body   Body          // transaction's body
-	done   chan struct{} // done channel is closed each time the transaction ends
-	result interface{}   // transaction's result
-	err    error         // if any error does happens while transaction execution it's here saved
+	body    Body          // transaction's body
+	checked bool          // determines if the precondition has passed of not
+	done    chan struct{} // done channel is closed each time the transaction ends
+	result  interface{}   // transaction's result
+	err     error         // if any error does happens while transaction execution it's here saved
 }
 
 func (tx *transaction) hasFinished() {
 	defer close(tx.done)
 
-	if p := recover(); p != nil {
+	if !tx.checked {
+		return
+	} else if p := recover(); p != nil {
 		tx.err = fmt.Errorf(ErrTransactionPanic, p)
 		tx.body.Rollback()
 	} else if tx.err != nil {
 		tx.body.Rollback() // err is non-nil; don't change it
 	} else {
-		tx.err = tx.body.Commit() // if Commit returns error update err with commit err
+		tx.err = tx.body.Commit()
 	}
+}
+
+func (tx *transaction) checkBody() bool {
+	if tx.err = tx.body.Precondition(); tx.err != nil {
+		return false
+	}
+
+	return true
 }
 
 func (tx *transaction) Execute(ctx context.Context) {
 	defer tx.hasFinished()
 	tx.done = make(chan struct{})
 
-	if tx.err = tx.body.Precondition(); tx.err != nil {
+	if checked := tx.checkBody(); !checked {
 		return
 	}
 
