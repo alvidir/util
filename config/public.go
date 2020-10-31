db2100187bbb835b6feb3175d023781d3f32f452
Hèctor Morales <thehector1593@gmail.com>
2020-10-31 01:52:03 +0100

    added: config package: .env utils

--- config/public.go
+++ config/public.go
@@ -5,11 +5,25 @@ import (
 	"os"
 )
 
-// CheckEnv looks up for the env variable key and returns it if exists, els err != nil
+// CheckEnv looks up for the env variable key and returns it if exists, else err != nil
 func CheckEnv(key string) (value string, err error) {
 	value, exists := os.LookupEnv(key)
 	if !exists {
 		err = fmt.Errorf(errEnvNotFound, key)
+	}
+
+	return
+}
+
+// CheckAllEnv looks up for all the env keys and returns an slice of its values, if exists, else err != nil
+func CheckAllEnv(keys ...string) (values []string, err error) {
+	values = make([]string, len(keys))
+	for index, key := range keys {
+		var exists bool
+		if values[index], exists = os.LookupEnv(key); !exists {
+			err = fmt.Errorf(errEnvNotFound, key)
+			return
+		}
 	}
 
 	return
