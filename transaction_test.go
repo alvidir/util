dbf03a6ceddc7546b537748ba5f6b879f29f9e72
Hèctor <thehector1593@gmail.com>
2021-06-22 21:39:56 +0200

    tests updates

--- transaction_test.go
+++ transaction_test.go
@@ -2,21 +2,29 @@ package util
 
 import (
 	"context"
-	"fmt"
+	"testing"
 )
 
-func ExampleTransaction_minimal() {
+func TestTransaction_minimal(t *testing.T) {
+	want := "hello world"
+	executed := false
+
 	builder := NewTransactionBuilder("testing", func(Tx) (interface{}, error) {
-		fmt.Println("Executing postcondition")
-		return nil, nil
+		executed = true
+		return want, nil
 	})
 
 	ctx := context.Background()
-	run := builder.Build().Execute(ctx)
+	job := builder.Build().Execute(ctx)
 
 	// wait for the transaction to finish
-	<-run.Done()
+	<-job.Done()
 
-	// Output:
-	// Executing postcondition
+	if !executed {
+		t.Errorf("Got postcondition execution %v, want %v", executed, true)
+	}
+
+	if got, err := job.Get(); err != nil || got != want {
+		t.Errorf("Got from transaction's result %v, %v, want %v, %v", got, err, want, nil)
+	}
 }
