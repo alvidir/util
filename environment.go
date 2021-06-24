059c5dc276389d582ffabab0d3c605bbf544f1b1
Hèctor <thehector1593@gmail.com>
2021-06-24 15:54:11 +0200

    updates

--- environment.go
+++ environment.go
@@ -20,35 +20,11 @@ func LookupEnv(key string) (env string, err error) {
 	return
 }
 
-// LookupAllEnv looks up for all the env keys and returns a map of values for each key, if exists, else err != nil
-func LookupAllEnv(keys ...string) (env map[string]string, err error) {
-	env = make(map[string]string)
-	for _, key := range keys {
-		var exists bool
-		if env[key], exists = os.LookupEnv(key); !exists {
-			err = fmt.Errorf(errEnvNotFound, key)
-			return
-		}
-	}
-
-	return
-}
-
-// LookupNempEnv looks up for all the env keys and returns a map of values for each key, if exists and not empty, else err != nil
-func LookupNempEnv(keys ...string) (env map[string]string, err error) {
-	env = make(map[string]string, len(keys))
-	for _, key := range keys {
-		var exists bool
-		if env[key], exists = os.LookupEnv(key); !exists {
-			err = fmt.Errorf(errEnvNotFound, key)
-			return
-		}
-
-		if len(env[key]) == 0 {
-			delete(env, key)
-			err = fmt.Errorf(errEnvIsEmpty, key)
-			return
-		}
+// LookupNempEnv looks up for the env variable key and returns it if exists and is not null, else err != nil
+func LookupNempEnv(key string) (env string, err error) {
+	var exists bool
+	if env, exists = os.LookupEnv(key); !exists || len(env) == 0 {
+		err = fmt.Errorf(errEnvNotFound, key)
 	}
 
 	return
